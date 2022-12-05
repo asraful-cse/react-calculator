ef8ef4f3835c82406361a355f5e8db0c5d9f07f2
asraful196 <asraful.01110@gmail.com>
2022-12-05 13:39:44 +0600

    update and dry code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import buttons from "./lib/data.json";
 const App = () => {
   const [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
+    setResult(result.concat(e));
   };
 
   const clear = () => {
@@ -14,6 +15,13 @@ const App = () => {
   const backspace = () => {
     // setResult(result.slice(0, result.length -1));
     setResult(result.slice(0, -1));
+  };
+
+  const positiveNegative = () => {
+    setResult(result * -1);
+  };
+  const percentage = () => {
+    setResult(result / 100);
   };
 
   const calculate = () => {
@@ -30,63 +38,39 @@ const App = () => {
         <from>
           <input type="text" value={result} />
         </from>
-
-        <div className="keypad">
-          <button onClick={clear} id="clear">
-            A/C
-          </button>
-          <button onClick={backspace} id="backspace">
-            CE
-          </button>
-          <button className="highlight" name="/" onClick={handleClick}>
-            &divide;
-          </button>
-          <button name="7" onClick={handleClick}>
-            7
-          </button>
-          <button name="8" onClick={handleClick}>
-            8
-          </button>
-          <button name="9" onClick={handleClick}>
-            9
-          </button>
-          <button className="highlight" name="*" onClick={handleClick}>
-            &times;
-          </button>
-          <button name="4" onClick={handleClick}>
-            4
-          </button>
-          <button name="5" onClick={handleClick}>
-            5
-          </button>
-          <button name="6" onClick={handleClick}>
-            6
-          </button>
-          <button className="highlight" name="-" onClick={handleClick}>
-            &ndash;
-          </button>
-          <button name="1" onClick={handleClick}>
-            1
-          </button>
-          <button name="2" onClick={handleClick}>
-            2
-          </button>
-          <button name="3" onClick={handleClick}>
-            3
-          </button>
-          <button className="highlight" name="+" onClick={handleClick}>
-            +
-          </button>
-          <button name="0" onClick={handleClick} id="result">
-            0
-          </button>
-          <button name="." onClick={handleClick}>
-            .
-          </button>
-          <button className="highlight" onClick={calculate}>
-            =
-          </button>
-        </div>
+        {buttons.length > 0 ? (
+          <div className="keypad">
+            {buttons.map((item) => (
+              <button
+                className={`${item.symbolName === "0" ? "result" : ""} ${
+                  item.symbolName === "/" ||
+                  item.symbolName === "*" ||
+                  item.symbolName === "+" ||
+                  item.symbolName === "-" ||
+                  item.symbolName === "="
+                    ? "highlight"
+                    : ""
+                }`}
+                onClick={() =>
+                  item.functionName === "clear"
+                    ? clear()
+                    : item.functionName === "positiveNegative"
+                    ? positiveNegative()
+                    : item.functionName === "handleClick"
+                    ? handleClick(item.symbolName)
+                    : item.functionName === "percentage"
+                    ? percentage()
+                    : item.functionName === "calculate"
+                    ? calculate()
+                    : null
+                }
+                id={item.id}
+              >
+                {item.symbolName}
+              </button>
+            ))}
+          </div>
+        ) : null}
       </div>
     </>
   );
